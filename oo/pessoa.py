c055f9666c8b783b00225f0d3a6f6f0709cbc063
Lionofpride <Lion21130218>
2020-08-29 00:11:08 -0300

    Criado sobreescrita de atributos de classe e sobreescrita de método de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -19,7 +19,10 @@ class Pessoa:
 
 
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
 
 class Mutante(Pessoa):
     olhos = 3
@@ -53,3 +56,5 @@ if __name__ == '__main__':
     print(isinstance(levon, Pessoa))
     print(isinstance(levon, Homem))
     print(levon.olhos)
+    print(levon.cumprimentar())
+    print(moreira.cumprimentar())
